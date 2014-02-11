d36f951c83223c1e72162701a59e715769226bba
Bruno Bergher <bruno.bergher@lookout.com>
2014-02-11 14:51:39 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-framer-fit
+framer.fit
 ==========
 
-Framer Resize to Fit
+Makes sure your Framer prototype fits the available screen dimensions
+
+# Usage
+
+1. Include the file in your HTML page
+2. After setting up your prototype, call ``utils.resizeToFit()``
+3. Done.
---
+++ framer.fit.js
@@ -0,0 +1,53 @@
+/*
+ * Framer.Fit 0.1 (2014-02-11)
+ * © 2014 Bruno Bergher
+ * Free to use under terms of MIT license
+ */
+
+utils.resizeToFit = function() {
+  var root = document.getElementById("FramerRoot");
+
+  var resizer = function() {
+    var content, available, scales, scale;
+
+    scales = { x: 1, y: 1 }
+
+    content = {
+      height: root.firstChild.clientHeight,
+      width: root.firstChild.clientWidth
+    }
+
+    available = {
+      height: window.innerHeight,
+      width: window.innerWidth
+    }
+
+    if(content.width > available.width) scales.x = available.width/content.width;
+    if(content.height > available.height) scales.y = available.height/content.height;
+    scale = Math.min(scales.x, scales.y);
+
+    console.log(available, content, scale)
+
+    if(scale < 1) {
+      document.body.style['-webkit-transform'] = "scale(" + scale + ")";
+      document.body.style['-moz-transform'] = "scale(" + scale + ")";
+      document.body.style['-ms-transform'] = "scale(" + scale + ")";
+      document.body.style['-o-transform'] = "scale(" + scale + ")";
+      document.body.style['transform'] = "scale(" + scale + ")";
+
+      document.body.style['-webkit-transform-origin'] = "0 0";
+      document.body.style['-moz-transform-origin'] = "0 0";
+      document.body.style['-ms-transform-origin'] = "0 0";
+      document.body.style['-o-transform-origin'] = "0 0";
+      document.body.style['transform-origin'] = "0 0";
+
+      document.body.style.overflow = "hidden";
+    }
+  }
+
+  if(root) {
+    resizer();
+  } else {
+    utils.delay(50, utils.resizeToFit);
+  }
+}
